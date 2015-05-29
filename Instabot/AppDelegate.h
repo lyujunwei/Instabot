88c6d67268d1d589d8a91a17a197ff63489afe0e
lvjunwei <lvjunwei@117go.com>
2015-05-29 14:33:02 +0800

    更新架构

---
+++ Instabot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instabot
+//
+//  Created by Junwei Lyu on 15/5/29.
+//  Copyright (c) 2015年 Junwei Lyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Instabot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instabot
+//
+//  Created by Junwei Lyu on 15/5/29.
+//  Copyright (c) 2015年 Junwei Lyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
